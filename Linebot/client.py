4a4b3536dee35e35825d3e05fceab1ccd2829589
lattalab <91266449+lattalab@users.noreply.github.com>
2024-06-30 22:40:07 +0800

    Update client.py

--- Linebot/client.py
+++ Linebot/client.py
@@ -7,14 +7,14 @@ def read_file(file_path):   # 讀取檔案內容
 
 def send_text_to_server(text):
     # 這是你要上傳資料的網址
-    url = 'https://ml-project-1r0x.onrender.com/receive_text'
+    url = 'your_website url'
     data = {'text': text}
     response = requests.post(url, json=data)
     print(response.json())
 
 if __name__ == "__main__":
     # 更改你的檔案路徑
-    file_path = 'C:/Users/vlva8/OneDrive/Desktop/test.txt'
+    file_path = 'your file path'
     # 得到檔案內容
     content = read_file(file_path)
     # 上傳到Linebot Webhook伺服器
